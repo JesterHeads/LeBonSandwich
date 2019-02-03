88673d94ae83efa2e5ffbee5ddca681ad5f38078
zink anthony <anthony@MBP.local>
2019-02-03 14:27:17 +0100

    ajout deleteMapping suivi de commande

--- suivi de commande/src/main/java/org/lpro/leBonSandwich/boundaries/CommandeRepresentation.java
+++ suivi de commande/src/main/java/org/lpro/leBonSandwich/boundaries/CommandeRepresentation.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 
 import org.lpro.leBonSandwich.entity.Commande;
 import org.lpro.leBonSandwich.exception.BadRequest;
+import org.lpro.leBonSandwich.exception.MethodNotAllowed;
 import org.lpro.leBonSandwich.exception.NotFound;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,6 +22,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -100,10 +102,13 @@ public class CommandeRepresentation {
                 }).orElseThrow(() -> new NotFound("Intervenant inexistant"));
     }
 
-//    @DeleteMapping(value="/id")
-//    public ResponseEntity<?> deleteCommande(@PathVariable("id") String id) throws NotFound,MethodNotAllowed{
-//
-//    }
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<?> deleteCommande(@PathVariable("id") String id) {
+        Commande commande = cr.findById(id).orElseThrow(() -> new NotFound("Commande inexistante"));
+        cr.delete(commande);
+        return new ResponseEntity<>(commande, HttpStatus.OK);
+    }
+
 }
 
 
